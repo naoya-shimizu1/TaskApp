8052c61379236de42617c3c4d8f537a1c55d3501
naoya.shimizu <Naoya_Shimizu@jrass.jp>
2024-04-12 13:30:03 +0900

    Initial Commit

---
+++ TaskApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskApp
+//
+//  Created by PC-SYSKAI555 on 2024/04/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
