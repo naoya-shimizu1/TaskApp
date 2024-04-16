a91e024664ef95445dd1d7a2316b03dd6b91caf3
naoya.shimizu <Naoya_Shimizu@jrass.jp>
2024-04-16 16:53:38 +0900

    first commit

---
+++ TaskApp/InputViewController.swift
@@ -0,0 +1,32 @@
+//
+//  InputViewController.swift
+//  TaskApp
+//
+//  Created by PC-SYSKAI555 on 2024/04/12.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
